7e0a3842dd14b9c0b6a49c53c7d3744fcacb7997
Kevin van der Burg <kevin.van.der.burg@freshheads.com>
2023-06-19 13:31:23 +0200

    Init with create-react-native-library

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  HttpClientExample
+//
+
+import Foundation
---
+++ ios/HttpClient.swift
@@ -0,0 +1,8 @@
+@objc(HttpClient)
+class HttpClient: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
