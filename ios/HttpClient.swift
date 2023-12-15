5f47182ff945274dd4c5f0c5f51473ad9d2b5775
Kevin van der Burg <vanderburg.kevin@gmail.com>
2023-12-15 10:57:00 +0100

    Added requiresMainQueueSetup False

--- ios/HttpClient.swift
+++ ios/HttpClient.swift
@@ -1,6 +1,10 @@
 @objc(HttpClient)
 class HttpClient: NSObject {
 
+  @objc override static func requiresMainQueueSetup() -> Bool {
+    return false
+  }
+
   @objc(multiply:withB:withResolver:withRejecter:)
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
     resolve(a*b)
@@ -9,11 +13,11 @@ class HttpClient: NSObject {
   @objc(get:optionsJson:resolver:rejecter:)
   func get(_ url: String, optionsJson: String?, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
     var components = URLComponents(string: url)
-    
+
     if let optionsJson = optionsJson {
       let optionsData = Data(optionsJson.utf8)
       if let options = try? JSONSerialization.jsonObject(with: optionsData, options: .mutableContainers) as? [String: Any] {
-        
+
         if let params = options["params"] as? [String: String] {
           components?.queryItems = params.map { URLQueryItem(name: $0.key, value: $0.value) }
         }
@@ -31,7 +35,7 @@ class HttpClient: NSObject {
     if let optionsJson = optionsJson {
       let optionsData = Data(optionsJson.utf8)
       if let options = try? JSONSerialization.jsonObject(with: optionsData, options: .mutableContainers) as? [String: Any] {
-        
+
         if let headers = options["headers"] as? [String: String] {
           for (key, value) in headers {
             request.setValue(value, forHTTPHeaderField: key)
@@ -45,7 +49,7 @@ class HttpClient: NSObject {
         reject("E_GET_FAILED", error.localizedDescription, error)
         return
       }
-      
+
       if let response = response as? HTTPURLResponse {
         var responseHeaders = [String: String]()
         for (key, value) in response.allHeaderFields {
@@ -53,12 +57,12 @@ class HttpClient: NSObject {
             responseHeaders[key] = value
           }
         }
-        
+
         var body = ""
         if let data = data {
             body = String(data: data, encoding: .utf8)?.trimmingCharacters(in: CharacterSet(charactersIn: "\u{0}")) ?? ""
         }
-        
+
         let jsonResult: [String: Any] = [
           "statusCode": response.statusCode,
           "requestHeaders": request.allHTTPHeaderFields ?? [:],
@@ -72,7 +76,7 @@ class HttpClient: NSObject {
         } else {
           reject("E_JSON_SERIALIZATION_FAILED", "Failed to serialize json", nil)
         }
-        
+
       } else {
         reject("E_NO_RESPONSE", "No response received", nil)
       }
